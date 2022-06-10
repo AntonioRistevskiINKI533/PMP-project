de1bbba57ef9191fc77fb385df90bb62dbeb0dde
AntonioRistevskiINKI533 <antonio.ristevski@uklo.edu.mk>
2022-06-10 20:30:25 +0200

    register works

---
+++ app/src/main/java/com/android/example/pmpproject/User.java
@@ -0,0 +1,15 @@
+package com.android.example.pmpproject;
+
+public class User {
+
+    public String email;
+
+    public User(){
+
+    }
+
+    public User(String email){
+        this.email=email;
+    }
+
+}
