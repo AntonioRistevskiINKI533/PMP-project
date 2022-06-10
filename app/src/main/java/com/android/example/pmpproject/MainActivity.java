a910475f4617f036f2daf76ab73c53b8d04e4610
AntonioRistevskiINKI533 <antonio.ristevski@uklo.edu.mk>
2022-06-10 17:19:57 +0200

    validators register

--- app/src/main/java/com/android/example/pmpproject/MainActivity.java
+++ app/src/main/java/com/android/example/pmpproject/MainActivity.java
@@ -2,12 +2,14 @@ package com.android.example.pmpproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView register;
 
@@ -17,5 +19,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.register:
+                startActivity(new Intent(this,RegisterActivity.class));
+                break;
+        }
+    }
+
 }
--- app/src/main/java/com/android/example/pmpproject/RegisterActivity.java
+++ app/src/main/java/com/android/example/pmpproject/RegisterActivity.java
@@ -2,13 +2,83 @@ package com.android.example.pmpproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView goBackToLogin, registerUser;
+    private EditText editTextEmail, editTextPassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        goBackToLogin = (TextView) findViewById(R.id.goBackToLogin);
+        goBackToLogin.setOnClickListener(this);
+
+        registerUser = (Button) findViewById(R.id.register);
+        registerUser. setOnClickListener(this);
+
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.goBackToLogin:
+                startActivity(new Intent(this,MainActivity.class));
+                break;
+            case R.id.register:
+                register();
+                break;
+        }
+    }
+
+    private void register() {
+        String email= editTextEmail.getText().toString().trim();
+        String password= editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please provide valid email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 8){
+            editTextPassword.setError("Min password length should be 8 characters!");
+            editTextPassword.requestFocus();
+            return;
+        }
     }
 }
